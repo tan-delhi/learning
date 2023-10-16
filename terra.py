0278cce48d65b32eb2ebf363e9ca9c0f3280fe63
tan-delhi <84720753+tan-delhi@users.noreply.github.com>
2023-10-17 02:15:33 +0530

    Update terra.py

--- terra.py
+++ terra.py
@@ -3,6 +3,6 @@ print("ASD")
 print("what is this , this is the third message")
 
 print("stashing a commit")
-print("are we onin branch")
+print("are we onin alpha")
 
 print("this is an awesome feature")

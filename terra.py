d2dceea4a458344642f88d28ed489d403ed7c6d9
tan-delhi <84720753+tan-delhi@users.noreply.github.com>
2023-10-17 02:08:15 +0530

    Update terra.py

--- terra.py
+++ terra.py
@@ -3,6 +3,6 @@ print("ASD")
 print("what is this , this is the third message")
 
 print("stashing a commit")
-print("are we on main branch")
+print("are we on main newer")
 
 print("this is an awesome feature")

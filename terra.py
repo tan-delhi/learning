47e7de14077066a84834dfb32006f3d24ed1e9f3
Tanmaiy Sethia Jain <f20190117@goa.bits-pilani.ac.in>
2024-01-20 21:16:57 +0530

    ho kyaa

--- terra.py
+++ terra.py
@@ -5,4 +5,4 @@ print("what is this , this is the third message")
 print("stashing a commit")
 print("are we on main newer")
 
-print("this is an awesome feature")
+print("this is an awesome feasdafature")
